dce66b196148d13b69583a5401ad4deab5309443
Pedro Gallego <pedropablo1996@gmail.com>
2019-08-31 16:51:44 -0500

    Inicio con proyecto vacio

---
+++ src/main/java/com/sura/misAliados/IndependientesColpensionesApplication.java
@@ -0,0 +1,13 @@
+package com.sura.misAliados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndependientesColpensionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndependientesColpensionesApplication.class, args);
+	}
+
+}
